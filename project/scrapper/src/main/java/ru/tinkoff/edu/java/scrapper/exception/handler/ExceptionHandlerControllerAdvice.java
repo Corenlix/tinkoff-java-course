f70211534ad7103b4a0f4da43bf212f599dc7f0e
corenlix <corenlix@gmail.com>
2023-03-21 00:16:26 +0300

    add exceptions into handlers

--- project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/exception/handler/ExceptionHandlerControllerAdvice.java
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/exception/handler/ExceptionHandlerControllerAdvice.java
@@ -1,9 +1,13 @@
 package ru.tinkoff.edu.java.scrapper.exception.handler;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import ru.tinkoff.edu.java.scrapper.dto.ApiErrorResponse;
 import ru.tinkoff.edu.java.scrapper.exception.ChatNotFoundException;
 import ru.tinkoff.edu.java.scrapper.exception.LinkNotFoundException;
@@ -24,9 +28,20 @@ public class ExceptionHandlerControllerAdvice {
         return new ApiErrorResponse("Чат не существует", HttpStatus.NOT_FOUND.toString(), ex);
     }
 
-    @ExceptionHandler(value = { Exception.class })
+    @ExceptionHandler(value = {
+            MethodArgumentTypeMismatchException.class,
+            HttpMessageNotReadableException.class,
+            HttpMediaTypeNotSupportedException.class,
+            MissingRequestHeaderException.class,
+    })
     @ResponseStatus(value = HttpStatus.BAD_REQUEST)
     public ApiErrorResponse badRequest(Exception ex) {
+        return new ApiErrorResponse("Некорректные параметры запроса", HttpStatus.BAD_REQUEST.toString(), ex);
+    }
+
+    @ExceptionHandler(value = { Exception.class })
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    public ApiErrorResponse serverError(Exception ex) {
         return new ApiErrorResponse("Некорректные параметры запроса", HttpStatus.BAD_REQUEST.toString(), ex);
     }
 }
