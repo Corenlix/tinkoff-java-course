936af18528be8800e4640d3d9838f9bbf3a16477
Corenlix <58521600+Corenlix@users.noreply.github.com>
2023-05-01 18:34:32 +0300

    add scrapper queue producer

---
+++ project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/ScrapperQueueProducer.java
@@ -0,0 +1,24 @@
+package ru.tinkoff.edu.java.scrapper.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+import ru.tinkoff.edu.java.scrapper.configuration.ApplicationConfig;
+import ru.tinkoff.edu.java.scrapper.dto.client.tgBot.LinkUpdate;
+
+@Service
+public class ScrapperQueueProducer {
+    private final RabbitTemplate rabbitTemplate;
+    private final String exchangeName;
+    private final String routingKey;
+
+    public ScrapperQueueProducer(RabbitTemplate rabbitTemplate, ApplicationConfig config) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.exchangeName = config.rabbitExchangeName();
+        this.routingKey = config.rabbitRoutingKey();
+    }
+
+    public void sendUpdate(LinkUpdate update) {
+        rabbitTemplate.convertAndSend(exchangeName, routingKey, update);
+    }
+}
